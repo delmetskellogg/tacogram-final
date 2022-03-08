e7fd8d8d53a728de6d385ddc9dd51a3ae6f7b776
delmetskellogg <david.elmets@kellogg.northwestern.edu>
2022-03-08 16:24:32 +0000

    Ready to ship

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_one_attached :uploaded_image
+
+  <% if post.uploaded_image.attached? %>
+  <% else %>
+    <img src="<%post.image class="img-fluid">
 end
